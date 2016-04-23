1474825204c7c49e87ff83bb72607c25bc911390
Tomasz Siemienik <tomek.siemienik@gmail.com>
2016-04-23 19:52:48 +0100

    Supertest as a request object in Mocha tests

--- app.js
+++ app.js
@@ -43,7 +43,3 @@ if (!module.parent) {
     console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
   });
 }
-
-exports.closeServer = function() {
-    server.close();
-}
--- test/index.js
+++ test/index.js
@@ -2,18 +2,21 @@
  * Created by tomek on 23/04/16.
  */
 
-var request = require("request");
-var assert = require("assert");
+var request = require("supertest");
+var assert = require('assert');
+var app = require ("../app.js");
 var base_url = "http://ec2-52-27-184-247.us-west-2.compute.amazonaws.com:3001/";
-var application = require ("../app.js");
 
 describe("GET /", function(){
-    it ("returns 200 status code", function(){
-        request.get(base_url, function (error, response, body) {
-            application.closeServer();
-            assert.isEqual(200, response.statusCode);
-            //assert.isEqual("hooray! welcome to our api!", body);
-            done();
-        });
+    it ("returns 200 status code", function(done){
+        request(app)
+            .get("/")
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end(function(err, res) {
+                if (err) return done(err);
+                assert.equal("hooray! welcome to our api!", res.body);
+                done();
+            });
     });
 });
